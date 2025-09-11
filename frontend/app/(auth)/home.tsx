abd0b4e418fe1548eb86f17eb0817390d2ca2a71
Juliet Crane <jlc565@cornell.edu>
2025-09-11 19:28:49 -0400

    basic email password auth

---
+++ frontend/app/(auth)/home.tsx
@@ -0,0 +1,14 @@
+import auth from '@react-native-firebase/auth';
+import { Button, Text, View } from 'react-native';
+
+const Page = () => {
+  const user = auth().currentUser;
+
+  return (
+    <View>
+      <Text>Welcome back back {user?.email}</Text>
+      <Button title="Sign out" onPress={() => auth().signOut()} />
+    </View>
+  );
+};
+export default Page;
